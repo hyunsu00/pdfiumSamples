cf3ab15c819a4d4498a4c875e8af264d46c09109
hyunsu00 <hyun955807@naver.com>
2021-04-21 20:32:42 +0900

    pdfiumSamples 초기 파일 추가

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # pdfiumSamples
+
+## github 프로젝트
+
+[pdfium-binaries](https://github.com/bblanchon/pdfium-binaries)
+
+## 사용된 pdfium-binaries 버전
+
+[chromium/4475](https://github.com/bblanchon/pdfium-binaries/releases/tag/chromium%2F4475)
---
+++ pdfiumSample01/pdfiumSamples01.cpp
@@ -0,0 +1,22 @@
+﻿// pdfiumSamples01.cpp : 애플리케이션의 진입점을 정의합니다.
+//
+
+#include "pdfiumSamples.h"
+#include <fpdfview.h>
+
+using namespace std;
+
+int main()
+{
+	FPDF_LIBRARY_CONFIG config;
+	config.version = 2;
+	config.m_pUserFontPaths = NULL;
+	config.m_pIsolate = NULL;
+	config.m_v8EmbedderSlot = 0;
+
+	FPDF_InitLibraryWithConfig(&config);
+
+	FPDF_DestroyLibrary();
+
+	return 0;
+}
